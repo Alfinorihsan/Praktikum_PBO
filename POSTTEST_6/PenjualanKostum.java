7db7f0b5c65ce007d6c3a2e6e18c8f05b11d57fd
Alfinorihsan <=alfinorihsan@gmail.com>
2023-04-10 15:26:30 +0800

    update static

--- POSTTEST_6/PenjualanKostum.java
+++ POSTTEST_6/PenjualanKostum.java
@@ -2,52 +2,57 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public final class PenjualanKostum {
-    private final ArrayList<KostumCosplay> daftarKostum;
-    private final Scanner input;
+private final ArrayList<KostumCosplay> daftarKostum;
+private final Scanner input;
+private static int jumlahKostum = 0; // variabel static untuk menghitung jumlah kostum
 
-    public PenjualanKostum() {
-        daftarKostum = new ArrayList<KostumCosplay>();
-        input = new Scanner(System.in);
+public PenjualanKostum() {
+    daftarKostum = new ArrayList<KostumCosplay>();
+    input = new Scanner(System.in);
+}
+
+public final void tambahKostum(final KostumCosplay kostum) {
+    daftarKostum.add(kostum);
+    jumlahKostum++; // menambah jumlahKostum setiap kali kostum ditambahkan
+}
+
+public final void tambahKostum(final int id, final String nama, final int harga, final int kategori) {
+    KostumCosplay kostumBaru;
+    if (kategori == 1) {
+        kostumBaru = new KostumAnime(id, nama, harga, "Anime");
+    } else if (kategori == 2) {
+        kostumBaru = new KostumGame(id, nama, harga, "Game");
+    } else {
+        System.out.println("Pilihan kategori tidak valid.");
+        return;
     }
+    daftarKostum.add(kostumBaru);
+    jumlahKostum++; // menambah jumlahKostum setiap kali kostum ditambahkan
+}
 
-    public final void tambahKostum(final KostumCosplay kostum) {
-        daftarKostum.add(kostum);
+public final void tampilkanKostum() {
+    System.out.println("Daftar Kostum Cosplay:");
+    for (KostumCosplay kostum : daftarKostum) {
+        System.out.println("ID: " + kostum.getId());
+        System.out.println("Nama Kostum: " + kostum.getNama());
+        System.out.println("Kategori Kostum: " + kostum.getKategori());
+        System.out.println("Harga: " + kostum.getHarga());
     }
+    System.out.println("Jumlah Kostum: " + jumlahKostum); // menampilkan jumlahKostum
+    System.out.println();
+}
 
-    public final void tambahKostum(final int id, final String nama, final int harga, final int kategori) {
-        KostumCosplay kostumBaru;
-        if (kategori == 1) {
-            kostumBaru = new KostumAnime(id, nama, harga, "Anime");
-        } else if (kategori == 2) {
-            kostumBaru = new KostumGame(id, nama, harga, "Game");
-        } else {
-            System.out.println("Pilihan kategori tidak valid.");
+public final void hapusKostum(final int id) {
+    for (int i = 0; i < daftarKostum.size(); i++) {
+        if (daftarKostum.get(i).getId() == id) {
+            daftarKostum.remove(i);
+            System.out.println("Kostum dengan ID " + id + " telah dihapus.");
+            jumlahKostum--; // mengurangi jumlahKostum setiap kali kostum dihapus
             return;
         }
-        daftarKostum.add(kostumBaru);
     }
-
-    public final void tampilkanKostum() {
-        System.out.println("Daftar Kostum Cosplay:");
-        for (KostumCosplay kostum : daftarKostum) {
-            System.out.println("ID: " + kostum.getId());
-            System.out.println("Nama Kostum: " + kostum.getNama());
-            System.out.println("Kategori Kostum: " + kostum.getKategori());
-            System.out.println("Harga: " + kostum.getHarga());
-        }
-        System.out.println();
-    }
-
-    public final void hapusKostum(final int id) {
-        for (int i = 0; i < daftarKostum.size(); i++) {
-            if (daftarKostum.get(i).getId() == id) {
-                daftarKostum.remove(i);
-                System.out.println("Kostum dengan ID " + id + " telah dihapus.");
-                return;
-            }
-        }
-        System.out.println("Kostum dengan ID " + id + " tidak ditemukan.");
-    }
+    System.out.println("Kostum dengan ID " + id + " tidak ditemukan.");
+}
 
     public final void editKostum(final int id) {
         for (int i = 0; i < daftarKostum.size(); i++) {
